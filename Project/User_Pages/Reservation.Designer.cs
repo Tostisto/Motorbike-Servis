6b8e13d4bf90678a6c2869a796ea3a65c877c713
Jakub Czepiec <kubaczepiec@email.cz>
2022-03-25 19:26:18 +0100

    New page New page for servis motorbike

--- Project/User_Pages/Reservation.Designer.cs
+++ Project/User_Pages/Reservation.Designer.cs
@@ -34,7 +34,6 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -83,7 +82,6 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.Window;
             this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.textBox2);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
@@ -101,23 +99,12 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(273, 533);
+            this.button1.Location = new System.Drawing.Point(285, 496);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 8;
             this.button1.Text = "Book";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(0, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(372, 54);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "MotorBike - Name";
             // 
             // textBox2
             // 
@@ -125,7 +112,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(175, 377);
+            this.textBox2.Location = new System.Drawing.Point(187, 340);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(192, 150);
@@ -134,7 +121,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(58, 377);
+            this.label3.Location = new System.Drawing.Point(70, 340);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(111, 20);
             this.label3.TabIndex = 6;
@@ -143,7 +130,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 158);
+            this.label2.Location = new System.Drawing.Point(79, 121);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 20);
             this.label2.TabIndex = 5;
@@ -151,14 +138,14 @@
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(175, 158);
+            this.monthCalendar1.Location = new System.Drawing.Point(187, 121);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 4;
             // 
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(175, 119);
+            this.textBox1.Location = new System.Drawing.Point(187, 82);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(192, 27);
             this.textBox1.TabIndex = 3;
@@ -166,7 +153,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(135, 122);
+            this.label1.Location = new System.Drawing.Point(147, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 20);
             this.label1.TabIndex = 2;
@@ -175,7 +162,7 @@
             // motorbikeBrand
             // 
             this.motorbikeBrand.AutoSize = true;
-            this.motorbikeBrand.Location = new System.Drawing.Point(48, 87);
+            this.motorbikeBrand.Location = new System.Drawing.Point(60, 50);
             this.motorbikeBrand.Name = "motorbikeBrand";
             this.motorbikeBrand.Size = new System.Drawing.Size(121, 20);
             this.motorbikeBrand.TabIndex = 0;
@@ -185,7 +172,7 @@
             // 
             this.motorbike_combobox.DataSource = this.userBindingSource;
             this.motorbike_combobox.FormattingEnabled = true;
-            this.motorbike_combobox.Location = new System.Drawing.Point(175, 84);
+            this.motorbike_combobox.Location = new System.Drawing.Point(187, 47);
             this.motorbike_combobox.Name = "motorbike_combobox";
             this.motorbike_combobox.Size = new System.Drawing.Size(192, 28);
             this.motorbike_combobox.TabIndex = 1;
@@ -311,7 +298,6 @@
         private TableLayoutPanel tableLayoutPanel1;
         private Panel panel2;
         private Button button1;
-        private Label label4;
         private TextBox textBox2;
         private Label label3;
         private Label label2;
--- Project/User_Pages/Servis.Designer.cs
+++ Project/User_Pages/Servis.Designer.cs
@@ -28,19 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.brand_textBox = new System.Windows.Forms.TextBox();
+            this.model_textBox = new System.Windows.Forms.TextBox();
+            this.message_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.place = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.year_input = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.spz_textbox = new System.Windows.Forms.TextBox();
+            this.add_servis = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.year_input)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Brand";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Model";
+            // 
+            // brand_textBox
+            // 
+            this.brand_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.brand_textBox.Location = new System.Drawing.Point(198, 22);
+            this.brand_textBox.Name = "brand_textBox";
+            this.brand_textBox.Size = new System.Drawing.Size(191, 27);
+            this.brand_textBox.TabIndex = 2;
+            // 
+            // model_textBox
+            // 
+            this.model_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.model_textBox.Location = new System.Drawing.Point(198, 59);
+            this.model_textBox.Name = "model_textBox";
+            this.model_textBox.Size = new System.Drawing.Size(191, 27);
+            this.model_textBox.TabIndex = 3;
+            // 
+            // message_textBox
+            // 
+            this.message_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.message_textBox.Location = new System.Drawing.Point(196, 191);
+            this.message_textBox.Multiline = true;
+            this.message_textBox.Name = "message_textBox";
+            this.message_textBox.Size = new System.Drawing.Size(547, 167);
+            this.message_textBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Description";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(127, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pobočka";
+            // 
+            // place
+            // 
+            this.place.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.place.FormattingEnabled = true;
+            this.place.Location = new System.Drawing.Point(198, 146);
+            this.place.Name = "place";
+            this.place.Size = new System.Drawing.Size(191, 28);
+            this.place.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Model Year";
+            // 
+            // year_input
+            // 
+            this.year_input.Location = new System.Drawing.Point(196, 104);
+            this.year_input.Maximum = new decimal(new int[] {
+            2022,
+            0,
+            0,
+            0});
+            this.year_input.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.year_input.Name = "year_input";
+            this.year_input.Size = new System.Drawing.Size(56, 27);
+            this.year_input.TabIndex = 10;
+            this.year_input.Value = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(258, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "SPZ";
+            // 
+            // spz_textbox
+            // 
+            this.spz_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.spz_textbox.Location = new System.Drawing.Point(298, 104);
+            this.spz_textbox.Name = "spz_textbox";
+            this.spz_textbox.Size = new System.Drawing.Size(89, 27);
+            this.spz_textbox.TabIndex = 12;
+            // 
+            // add_servis
+            // 
+            this.add_servis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.add_servis.Location = new System.Drawing.Point(198, 388);
+            this.add_servis.Name = "add_servis";
+            this.add_servis.Size = new System.Drawing.Size(119, 29);
+            this.add_servis.TabIndex = 13;
+            this.add_servis.Text = "Add request";
+            this.add_servis.UseVisualStyleBackColor = true;
             // 
             // Servis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.add_servis);
+            this.Controls.Add(this.spz_textbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.year_input);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.place);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.message_textBox);
+            this.Controls.Add(this.model_textBox);
+            this.Controls.Add(this.brand_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Servis";
             this.Text = "Servis";
+            ((System.ComponentModel.ISupportInitialize)(this.year_input)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox brand_textBox;
+        private TextBox model_textBox;
+        private TextBox message_textBox;
+        private Label label3;
+        private Label label4;
+        private ComboBox place;
+        private Label label5;
+        private NumericUpDown year_input;
+        private Label label6;
+        private TextBox spz_textbox;
+        private Button add_servis;
     }
 }
